2ca29ff193fac8798982db016d37016a885f86f7
Gopinath <kgopinath730@gmail.com>
2026-03-17 10:59:18 +0530

    retreival + generation success

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ DATASET_FILE_PATH = "database/customers.csv"
 
 EMBEDDING_MODEL = "text-embedding-3-small"
 
-VECTOR_DB_PATH = "/vectordb"
+VECTOR_DB_PATH = "./vectordb"
 
 LLM_MODEL="" #gpt-3.5-turbo 
 
@@ -21,9 +21,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class AgentState(TypedDict):
     prompt: str
+    context: str
+    query: str
     messages: Annotated[list[AnyMessage], add]
 
 # -----------------------------------------------------------------
@@ -32,38 +33,53 @@ class AgentState(TypedDict):
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage,HumanMessage
 
-from langchain_core.prompts import ChatPromptTemplate 
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 
 model = ChatOpenAI()
 
-# prompt = ChatPromptTemplate.from_messages(
-#     ("system", "")
-# )
+system_prompt = """
+You are a professional banking agent of bank X. You are responsible for answering the customer queries related to their bank account they hold in the bank X. the customers are logged in to the app already. you can provide with dial info they ask. they have completed their verification.
 
-# ----------------------------------------------------------------
-# Nodes
-# ------------------------------------------------------------
+you are provided with appropriate context loaded from the database of the bank customers. Use the context Related to the appropriate customers who query about their accounts. to answer their queries clearly and concisely.
 
-def chat_node(state:AgentState):
-    return {
-        "messages":[
-            model.invoke(
-                [
-                    SystemMessage(content="you are an helpful assistant")
-                ]
-                + state["messages"]
+CRITICAL RULES
+> Do not provide false information to the users. 
+> Do not provide information other than the user themselves.
+> Do not hallucinate while giving replies to the customer.
+> Don't share information of the other customers to another customers
 
-                + [HumanMessage(content=state["prompt"])]
-            )
-        ]
-    }
-# ---------------------------------------------------------
+EXAMPLE 1
+
+User: I am Shreya Nair, hello
+System: hi Shreya Nair, how's your day going on? How can i assist you ?
+User: I need current account balance
+System: Sure, your account balance is INR 23,300.00 on June 3rd.
+
+EXAMPLE 2
+
+User: I am swrup shinde
+System: How may i assist you swrup
+User: I need my last transaction details
+System: I am now providing your last transaction details, Credit 3000 from Account no. ending xx3439 from mahinder sharma. 
+
+
+Be responsible and Provide true information to the customers.
+
+"""
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),
+    MessagesPlaceholder(variable_name="history"),
+    ("user", "{user_query}\n\n context: {context} \n\n query:{query}")
+])
+
+chain = prompt_template | model
+
+#  ---------------------------------------------------------
 # DATASET PROCESSING & EMBEDDING/VECTOR STORE CREATION
 # ---------------------------------------------------------
 import pandas as pd
-
-
 
 dataset = pd.read_csv(DATASET_FILE_PATH)
 
@@ -119,41 +135,72 @@ def create_embeddings(chunks:list[str]):
 
 
 vector_store = FAISS.load_local(
-    r"/vectordb",
+    r"./vectordb",
     embeddings,
     allow_dangerous_deserialization=True
     )
 
-def vector_search(query:str):
-    return vector_store.similarity_search(query, k=1)
+def vector_search(query:str, k=3):
+    return vector_store.similarity_search(query, k=k)
 
 
+
+
+# ----------------------------------------------------------------
+# Nodes
+# ------------------------------------------------------------
+
+# Chat node
+def chat_node(state:AgentState):
+    response = chain.invoke({
+        "user_query": state["prompt"],
+        "context": state["context"],
+        "query": state["query"],
+        "history": state["messages"]
+    })
+    return {"messages": [response]}
+
+def retrieve_context(state:AgentState):
+    query = state["query"]
+    docs = vector_search(query)
+    context = "context: \n\n".join([doc.page_content for doc in docs])
+    print("fetched context")
+    return {"context": context}
+
+#
 # -------------------------------------------------------------------
 #  STATE COMPILATION
 # ----------------------------------------------------------------
 from langgraph.graph import START,END, StateGraph
+from langgraph.checkpoint.memory import MemorySaver
 
 graph = StateGraph(AgentState)
 
+graph.add_node("retrieve", retrieve_context)
 graph.add_node("chat", chat_node)
-graph.add_edge(START, "chat")
+
+graph.add_edge(START, "retrieve")
+graph.add_edge("retrieve", "chat")
 graph.add_edge("chat", END)
 
-app = graph.compile()
+memory = MemorySaver()
+app = graph.compile(checkpointer=memory)
 
 
 
 
 def main():
     print("Hello from customer-qa-assistant!")
-
-
+    config = {"configurable": {"thread_id": "session-1"}}
     while True:
-        inpt = input()
+        inpt = input("You: ")
         if inpt == ":":
             break
-        response = app.invoke({"prompt":inpt,"messages":[]})
-        print(response["messages"][-1].content)
+        response = app.invoke(
+            {"prompt": inpt, "context": "", "query": inpt, "messages": []},
+            config=config
+        )
+        print("Agent:", response["messages"][-1].content)
 
 
 
